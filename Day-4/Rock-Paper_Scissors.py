28fc1c491419fb9eabe27953c52ab809e3f7b1f1
NavinRaja K <63846133+navinfeb15@users.noreply.github.com>
2023-04-22 11:34:24 +0000

    initial commit

---
+++ Day-4/Rock-Paper_Scissors.py
@@ -0,0 +1,60 @@
+import random  # Import the random module
+
+# Define the three hand gestures as string variables
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+# Create a list containing the three hand gestures
+choice = [rock, paper, scissors]
+
+# Prompt the user to input their choice and print the corresponding gesture
+user_choice = int(input(
+    "What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+print("You chose\n", choice[user_choice])
+
+# Generate a random choice for the computer and print the corresponding gesture
+comp_choice = random.randint(0, 2)
+print("Computer chose:\n", choice[comp_choice])
+
+# Compare the user's choice with the computer's choice and print the result
+if user_choice == comp_choice:
+    print("Match draw...")
+elif user_choice == 0:
+    if comp_choice == 1:
+        print("You Lose ☹️")
+    elif comp_choice == 2:
+        print("You Win !")
+elif user_choice == 1:
+    if comp_choice == 0:
+        print("You Win !")
+    elif comp_choice == 2:
+        print("You Lose ☹️")
+elif user_choice == 2:
+    if comp_choice == 0:
+        print("You Lose ☹️")
+    elif comp_choice == 1:
+        print("You Win !")
