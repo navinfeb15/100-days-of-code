d237439ddd4d98bdef7065ad84f9cfd47778cd2d
NavinRaja K <63846133+navinfeb15@users.noreply.github.com>
2023-05-09 14:07:46 +0000

    added README files

---
+++ Day-20/README.md
@@ -0,0 +1,32 @@
+# Day-20
+
+
+# Snake Game
+
+This is a simple Snake  game  implemented in  Python  using the Turtle graphics library.
+
+## Requirements
+
+To run this game, you will need:
+
+-   Python 3.x
+-   The  `turtle`  module (included in Python's standard library)
+
+## How to Play
+
+1.  Run the  `snake_game.py`  file using Python.
+2.  Use the arrow keys to move the snake around the screen.
+3.  Try to eat as many food items as possible without running into the walls or the snake's own tail.
+4.  The game ends when the snake crashes into a wall or its own tail.
+
+## Customization
+
+You can customize this game by modifying the code in the  `snake.py`  file. For example, you can change the size of the  game board, the speed of the snake, or the appearance of the snake and food items.
+
+## Credits
+
+This game was inspired by the classic Snake game, which was first introduced in the late 1970s. The  Python implementation  and graphics were created by [Your Name Here].
+
+## License
+
+This project is licensed under the  MIT License  - see the LICENSE file for details.
--- Day-20/Snake game - part 1.py
+++ Day-20/Snake game - part 1.py
@@ -1,85 +1,33 @@
-from turtle import Turtle,Screen
-import random, time
+from turtle import Turtle, Screen
+import random
+import time
 from snake import Snake
+
+# Initialize the screen
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
-# user_choice = screen.textinput("Turtle reacing ", "Choose the color of turtle you want to bet on :")
 
-# colors = ['red', 'blue', 'yellow', 'green', 'orange', 'purple']
-# turtles_list = []
-# is_game_on = False
-
-
-# turtle_count = 0
-# class Race():
-  
-#   def __init__(self, turtle_color):
-#     self.turtle_color = turtle_color
-#     self.turtle = Turtle(shape="turtle")
-    
-#     self.turtle.color(turtle_color)
-#     self.turtle.penup()
-#     self.turtle.speed("fastest")
-    
-#     global turtle_count
-#     turtle_count += 1  
-#     self.goto_xy = (-280,90-30*turtle_count)
-
-#   def set_position(self, pos):
-#     self.turtle.goto(pos)    
-
-#   def forward(self,distance):
-#     self.turtle.forward(distance)
-#   def xcor(self):
-#     return self.turtle.xcor()
-
-
-# if user_choice:
-#   is_game_on =True
-    
-#   for color in colors:
-#     turtle_name = str(color)+"_turtle"
-#     turtle_name = Race(color)
-#     # print(color,":", turtle_name.goto_xy)
-    
-#     turtle_name.set_position(turtle_name.goto_xy)
-#     turtles_list.append(turtle_name)
-      
-#   while is_game_on:
-
-#     for turtle in turtles_list:
-#       if turtle.xcor() >= 265:
-#         # print(turtle.xcor())
-#         is_game_on=False
-        
-#         if user_choice == turtle.turtle_color:
-#           print(f"You won! The {turtle.turtle_color} won the Race ")
-#           # is_game_on=False
-#         else:
-#           print(f"You Lost! The {turtle.turtle_color} won the Race ")
-#         break
-      
-      
-#       dist = random.randint(10,20)
-#       turtle.forward(dist)
+# Turn off animation to speed up the game
 screen.tracer(0)
+
+# Initialize the game
 game_on = True
 snake = Snake()
- 
-screen.listen()
 
+# Listen for keyboard input
+screen.listen()
 screen.onkey(fun=snake.up, key="Up")
 screen.onkey(fun=snake.down, key="Down")
 screen.onkey(fun=snake.left, key="Left")
 screen.onkey(fun=snake.right, key="Right")
 
-while game_on:          
+# Start the game loop
+while game_on:
+    # Move the snake
     snake.move()
     time.sleep(0.5)
     screen.update()
 
-
-
+# Exit the game when the user clicks the screen
 screen.exitonclick()
-
