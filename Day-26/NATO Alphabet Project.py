cde440402c27ecd6be23878caf16535b76646d23
NavinRajaK <navinfeb15@gmail.com>
2023-05-28 20:58:20 +0530

    Initial commit

--- Day-26/NATO Alphabet Project.py
+++ Day-26/NATO Alphabet Project.py
@@ -4,7 +4,16 @@ data = pd.read_csv("Day-26/nato_phonetic_alphabet.csv")
 
 new_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
-user_words = list(input("Enter a word: "))
-final_word = [new_dict[word.upper()] for word in user_words]
 
-print(final_word)
+def nato_phonetic_alphabet():
+    user_words = list(input("Enter a word: "))
+    try:
+        final_word = [new_dict[word.upper()] for word in user_words]
+        
+    except KeyError:
+        print("Sorry only letters in the alphabet please.")
+        nato_phonetic_alphabet()
+    else:
+        print(final_word)
+
+nato_phonetic_alphabet()
