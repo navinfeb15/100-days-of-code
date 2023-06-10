189d740a54bace88041ffc0d8a0293c68f3f59a7
NavinRaja K <63846133+navinfeb15@users.noreply.github.com>
2023-06-10 06:02:22 +0000

    Initial commit

--- Day-35/rain_alert.py
+++ Day-35/rain_alert.py
@@ -1,6 +1,7 @@
 import requests
 import os
 from twilio.rest import Client
+from datetime import datetime
 
 
 # Your Twilio account details
@@ -8,7 +9,7 @@ account_sid = "AC70c2d9568e8697cc25467fb084e74034"
 auth_token = os.environ.get("TW_AUTH_TOKEN")
 
 # Your OpenWeatherMap API key
-api_key = os.environ.get("OWM_API_KEY")
+api_key = "69f04e4613056b159c2761a9d9e664d2"
 
 # Create a Twilio client object with your account_sid and auth_token
 client = Client(account_sid, auth_token)
@@ -23,22 +24,31 @@ parameters = {
 
 # Send a GET request to the OpenWeatherMap API with the specified parameters and retrieve the response
 response = requests.get(url, params = parameters)
-
 # Initialize a variable to track whether it will rain
 will_rain = False
 
 # Extract the weather data for the next 12 hours from the response
-hourly_sliced = response.json()["hourly"][0:12]
+start_time = 8
+raining_hours = ()
+hourly_sliced = response.json()["hourly"][start_time:20]
 
 # Check if it will rain in the next 12 hours
 for hour_data in hourly_sliced:
-    if int(hour_data["weather"][0]["id"]) < 700: # If the weather condition is less than 700, it means it will rain
+    hour_code = int(hour_data["weather"][0]["id"])
+    if hour_code < 700:  # if the weather condition is less than 700, it means it will rain
         will_rain = True
+        
+        time_str = datetime.strptime(f'{start_time}:00', '%H:%M').strftime('%-I %p')
+        raining_hours += (time_str,)
+    start_time += 1
+
+total_raining = str(raining_hours).replace("'", "")
+
 
 # If it will rain, send a message via Twilio to the specified phone number
-if will_rain:
-    message = client.messages.create(
-        body = "It's going to rain today. Remember to bring an ☂️",
-        from_ = '+14849699513', # Your Twilio phone number
-        to = '+917338961810' # The recipient's phone number
-    )
+# if will_rain:
+#     message = client.messages.create(
+#         body = f"It will rain today at {total_raining}. Take an ☂️🌦️⛈️",
+#         from_ = '+14849699513', # Your Twilio phone number
+#         to = '+917338961810' # The recipient's phone number
+#     )
