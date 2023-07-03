40c398d376f6c87293813367a3892771753ef4bb
NavinRaja K <63846133+navinfeb15@users.noreply.github.com>
2023-07-03 14:23:43 +0000

    Initial commit

--- Day-53/main.py
+++ Day-53/main.py
@@ -1,1 +1,40 @@
-GOOGLE_FORM_LINK = "https://forms.gle/Rk5uhoZStSYMWtbi6"
+from bs4 import BeautifulSoup
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
+
+
+
+
+
+class SfRentalResearch:
+
+    def __init__(self):
+        self.GOOGLE_FORM_LINK = "https://forms.gle/Rk5uhoZStSYMWtbi6"
+        self.SF_RENTAL_LINK = r"https://www.zillow.com/homes/for_rent/1-_beds/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22usersSearchTerm%22%3Anull%2C%22mapBounds%22%3A%7B%22west%22%3A-122.56276167822266%2C%22east%22%3A-122.30389632177734%2C%22south%22%3A37.69261345230467%2C%22north%22%3A37.857877098316834%7D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22pmf%22%3A%7B%22value%22%3Afalse%7D%2C%22pf%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A12%7D"
+            
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36")
+        prefs = {"profile.default_content_setting_values.notifications": 2}
+        chrome_options.add_experimental_option("prefs", prefs)
+        service = Service(executable_path='C:\Program Files\Chrome Driver\chromedriver.exe', chrome_options=chrome_options)
+        self.driver = webdriver.Chrome(service=service)
+        self.actions = ActionChains(self.driver)
+        
+        
+        response = requests.get(self.SF_RENTAL_LINK, headers).text
+        self.soup = BeautifulSoup(response, 'html.parser')
+        print(self.soup)
+        
+    def get_rental(self):
+        
+        self.driver
+        
+        
+        
+
+test = SfRentalResearch()
